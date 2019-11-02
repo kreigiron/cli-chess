035e148073753810a39305e32ff0ea962a9e8169
Erik Giron <1243595+kreigiron@users.noreply.github.com>
2019-11-01 20:27:33 -0600

    improve board rendering

--- src/main/java/rocks/kreig/chess/game/Board.java
+++ src/main/java/rocks/kreig/chess/game/Board.java
@@ -1,6 +1,13 @@
 package rocks.kreig.chess.game;
 
 class Board {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     private final Cell[][] cells = new Cell[64][64];
 
     public Board() {
@@ -17,13 +24,15 @@ class Board {
 
     @Override
     public String toString() {
-        String board = "+-------------------------------+\n";
+        String board = "\n";
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                board = board.concat(cells[i][j].isShaded() ? "| x " : "| o ");
+                board = board.concat((
+                        cells[i][j].isShaded() ?
+                                ANSI_BLACK_BACKGROUND + ANSI_WHITE + " x " :
+                                ANSI_WHITE_BACKGROUND + ANSI_BLACK + " x ") + ANSI_RESET);
             }
-            board = board.concat("|\n");
-            board = board.concat("+-------------------------------+\n");
+            board = board.concat("\n");
         }
         return board;
     }
